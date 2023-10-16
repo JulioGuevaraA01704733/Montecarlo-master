d39ac84a1e736f391106dc1dac67be3eab4547ce
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2023-10-15 20:01:07 -0600

    Add files via upload

---
+++ Satelite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montecarlo.Modelos
+{
+    public class Satelite
+    {
+        public int IdSatelite { get; set; }
+        public int NumPaneles { get; set; }
+
+        public List<int> ListaValores { get; set; }
+
+        public int Xi {  get; set; }  
+
+        public Satelite(int idSatelite, int numPaneles, List<int> listaValores, int xi)
+        {
+            IdSatelite = idSatelite;
+            NumPaneles = numPaneles;
+            ListaValores = listaValores;
+            Xi = xi;
+        }
+        public Satelite() 
+        {
+            ListaValores = new List<int>();
+        }
+
+    }
+}
